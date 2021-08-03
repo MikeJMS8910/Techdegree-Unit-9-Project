4cb5676a8474dac884b504f0da9ae71a3ea183f1
MikeJMS8910 <Michael.Sutu@gmail.com>
2021-08-03 12:16:24 -0400

    Update index.js

--- seed/index.js
+++ seed/index.js
@@ -2,13 +2,19 @@
 
 const promiseFinally = require('promise.prototype.finally');
 const Database = require('./database');
-const data = require('./data.json');
+const userData = require('./users.json');
+const courseData = require('./courses.json')
 
 const enableLogging = process.env.DB_ENABLE_LOGGING === 'true';
-const database = new Database(data, enableLogging);
+const userDatabase = new Database(userData, enableLogging);
+const coursesDatabase = new Database(courseData, enableLogging);
 
 promiseFinally.shim();
 
-database.init()
+userDatabase.init()
+  .catch(err => console.error(err))
+  .finally(() => process.exit());
+
+coursesDatabase.init()
   .catch(err => console.error(err))
   .finally(() => process.exit());
